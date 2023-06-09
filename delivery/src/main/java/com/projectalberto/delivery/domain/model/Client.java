7bd24f8702f050a4a4f79e9a52b4b9d7af893cb3
Alberto Felipe <felipe0032sena@gmail.com>
2023-06-09 18:05:01 -0300

    feat: Client table migration and Client model

---
+++ delivery/src/main/java/com/projectalberto/delivery/domain/model/Client.java
@@ -0,0 +1,27 @@
+package com.projectalberto.delivery.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
+public class Client {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+}
